1187ea107fd92a627ddc01e861a95e15fd919425
Víctor Hernández <alu0100697032@ull.edu.es>
2016-06-15 11:37:17 +0100

    Implementando login

--- UllApp/app/src/main/java/com/example/v/ullapp/LogIn.java
+++ UllApp/app/src/main/java/com/example/v/ullapp/LogIn.java
@@ -2,6 +2,9 @@ package com.example.v.ullapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by v on 07/04/2016.
@@ -11,5 +14,37 @@ public class LogIn extends AppCompatActivity{
     protected void onCreate(Bundle savedIntanceState){
         super.onCreate(savedIntanceState);
         setContentView(R.layout.log_in);
+    }
+
+    public void authenticate (View view){
+        EditText u = (EditText)findViewById(R.id.username);
+        EditText p = (EditText)findViewById(R.id.password);
+        String username = u.getText().toString();
+        String password = p.getText().toString();
+        TextView display = (TextView)findViewById(R.id.display);
+        display.setText("Username: " + username + " - Password: " + password);
+
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
