aa4e4e847ac08dcb59944081824e4a7fd069139a
Víctor Hernández <alu0100697032@ull.edu.es>
2016-06-16 22:31:58 +0100

    Implementing authentication with facebook

--- UllApp/app/src/main/java/com/example/v/ullapp/LogIn.java
+++ UllApp/app/src/main/java/com/example/v/ullapp/LogIn.java
@@ -1,50 +1,166 @@
 package com.example.v.ullapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
 
 /**
  * Created by v on 07/04/2016.
  */
 public class LogIn extends AppCompatActivity{
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private TextView btnLogin;
+    private ProgressDialog progressDialog;
+    User user;
     @Override
     protected void onCreate(Bundle savedIntanceState){
         super.onCreate(savedIntanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.log_in);
+
+        if(PrefUtils.getCurrentUser(LogIn.this) != null){
+
+            Intent homeIntent = new Intent(LogIn.this, LogOut.class);
+
+            startActivity(homeIntent);
+
+            finish();
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        callbackManager=CallbackManager.Factory.create();
+
+        loginButton= (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions("public_profile", "email","user_friends");
+
+        btnLogin= (TextView) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                progressDialog = new ProgressDialog(LogIn.this);
+                progressDialog.setMessage("Loading...");
+                progressDialog.show();
+
+                loginButton.performClick();
+
+                loginButton.setPressed(true);
+
+                loginButton.invalidate();
+
+                loginButton.registerCallback(callbackManager, mCallBack);
+
+                loginButton.setPressed(false);
+
+                loginButton.invalidate();
+
+            }
+        });
     }
 
-    public void authenticate (View view){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private FacebookCallback<LoginResult> mCallBack = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+
+            progressDialog.dismiss();
+
+            // App code
+            GraphRequest request = GraphRequest.newMeRequest(
+                    loginResult.getAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(
+                                JSONObject object,
+                                GraphResponse response) {
+
+                            Log.e("response: ", response + "");
+                            try {
+                                user = new User();
+                                user.facebookID = object.getString("id").toString();
+                                user.email = object.getString("email").toString();
+                                user.name = object.getString("name").toString();
+                                user.gender = object.getString("gender").toString();
+                                PrefUtils.setCurrentUser(user,LogIn.this);
+
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+                            Toast.makeText(LogIn.this,"Bienvenido "+user.name, Toast.LENGTH_LONG).show();
+                            Intent intent=new Intent(LogIn.this,LogOut.class);
+                            startActivity(intent);
+                            finish();
+
+                        }
+
+                    });
+
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,email,gender, birthday");
+            request.setParameters(parameters);
+            request.executeAsync();
+        }
+
+        @Override
+        public void onCancel() {
+            progressDialog.dismiss();
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+            progressDialog.dismiss();
+        }
+    };
+
+    public void authenticate (View view) throws IOException {
         EditText u = (EditText)findViewById(R.id.username);
         EditText p = (EditText)findViewById(R.id.password);
         String username = u.getText().toString();
         String password = p.getText().toString();
         TextView display = (TextView)findViewById(R.id.display);
-        display.setText("Username: " + username + " - Password: " + password);
-
-        final TextView mTextView = (TextView) findViewById(R.id.text);
-
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
-            }
-        });
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        //display.setText("Username: " + username + " - Password: " + password);
     }
 }
---
+++ UllApp/app/src/main/java/com/example/v/ullapp/User.java
@@ -0,0 +1,14 @@
+package com.example.v.ullapp;
+
+/**
+ * Created by Usuario on 16/06/2016.
+ */
+public class User {
+    public String name;
+
+    public String email;
+
+    public String facebookID;
+
+    public String gender;
+}
